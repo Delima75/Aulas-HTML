576788bb0621215f7a802c301372d75afac640ee
Delima75 <jorged.lima@gmail.com>
2022-01-26 08:01:27 -0300

    Initial commit

---
+++ Curso JS/teste-js/teste.js
@@ -0,0 +1,10 @@
+var Frutas = `{
+    "fruta":"banana",
+    "preço":15.50,
+    "tipo":["prata",
+            "maça",
+            "nanica", 
+            "da terra",
+            "ouro"],
+    "origem":"Brasil,
+}`
